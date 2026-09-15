b3b8ce0601da66f78c42d12710540be7b68d83be
qooqnos <qooqnos@gmail.com>
2026-09-15 11:51:28 +0330

    feat(database): export session repository

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -3,5 +3,6 @@ export * from "./transaction";
 export * from "./migrations";
 export * from "./migration-catalog";
 export * from "./identity-repository";
+export * from "./session-repository";
 export * from "./workspace-repository";
 export * from "./services";
