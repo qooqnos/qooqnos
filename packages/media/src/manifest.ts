c889593a5d24450ed837113c5d5298600bf31dd4
qooqnos <qooqnos@gmail.com>
2026-09-15 15:24:28 +0330

    feat(media): register media runtime module

---
+++ packages/media/src/manifest.ts
@@ -0,0 +1,8 @@
+import type { RuntimeModule } from "@qooqnos/runtime";
+
+export const MEDIA_MODULE: RuntimeModule = {
+  id: "media",
+  version: "0.1.0",
+  dependencies: [],
+  permissions: [],
+};
