8c7aa9c17d03c475b100ad3bcec7950973bda234
qooqnos <qooqnos@gmail.com>
2026-09-22 14:42:39 +0330

    Remove unused Billing service type import

--- packages/billing/src/service.ts
+++ packages/billing/src/service.ts
@@ -1,4 +1,3 @@
-import type { RequestContext } from "@qooqnos/core";
 import { DatabaseError } from "@qooqnos/database";
 import { BillingRepository } from "./repository";
 import type { BillingAIEntitlementDecision, BillingAIEntitlementRequest, BillingAIEntitlementService } from "./contract";
