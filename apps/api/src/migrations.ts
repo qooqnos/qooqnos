69c71e6a96d71f7503bacc165499728486c76fc8
qooqnos <qooqnos@gmail.com>
2026-09-23 08:05:40 +0330

    Register Fulfillment core migration

--- apps/api/src/migrations.ts
+++ apps/api/src/migrations.ts
@@ -40,6 +40,7 @@ import privacyConsentRequests from "../../../migrations/0039_privacy_consent_req
 import demandMatchingCore from "../../../migrations/0040_demand_matching_core.sql";
 import demandMatchingIntegrity from "../../../migrations/0041_demand_matching_integrity.sql";
 import reviewsModerationReputation from "../../../migrations/0048_reviews_moderation_reputation.sql";
+import fulfillmentCore from "../../../migrations/0049_fulfillment_core.sql";
 import reviewsCore from "../../../migrations/0042_reviews_core.sql";
 import integrityUpdateGuards from "../../../migrations/0043_integrity_update_guards.sql";
 import billingCounterScope from "../../../migrations/0044_billing_counter_scope.sql";
@@ -97,4 +98,5 @@ export const migrationSources: readonly MigrationSource[] = [
   { path: "migrations/0046_booking_finalization_guards.sql", sql: bookingFinalizationGuards },
   { path: "migrations/0047_booking_capacity_update_guards.sql", sql: bookingCapacityUpdateGuards },
   { path: "migrations/0048_reviews_moderation_reputation.sql", sql: reviewsModerationReputation },
+  { path: "migrations/0049_fulfillment_core.sql", sql: fulfillmentCore },
 ];
