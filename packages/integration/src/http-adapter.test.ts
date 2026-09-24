eb098178dae139ce4b43e5f852ae25d505213f89
qooqnos <qooqnos@gmail.com>
2026-09-24 17:29:30 +0330

    allow intentional test fixture type suppression

--- packages/integration/src/http-adapter.test.ts
+++ packages/integration/src/http-adapter.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 import { describe, expect, it } from "vitest";
 import { credentialEnvironmentKey, createEnvironmentCredentialResolver } from "./credential";
