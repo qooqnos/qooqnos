effd5fee4293a6f657d3f13828638e74a81d1acd
qooqnos <qooqnos@gmail.com>
2026-09-24 14:50:21 +0330

    feat(case-support): export external dispatch boundary

--- packages/case-support/src/index.ts
+++ packages/case-support/src/index.ts
@@ -1,3 +1,6 @@
 export * from "./repository";
 export * from "./service";
 export * from "./manifest";
+export * from "./dispatch-adapter";
+export * from "./dispatch-repository";
+export * from "./dispatch";
