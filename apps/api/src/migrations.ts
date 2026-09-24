321283dac72f259763dc726457b755577ce4ae69
qooqnos <qooqnos@gmail.com>
2026-09-24 18:04:45 +0330

    register SEO GEO migration

--- apps/api/src/migrations.ts
+++ apps/api/src/migrations.ts
@@ -71,6 +71,7 @@ import caseQueueProviderDispatch from "../../../migrations/0073_case_queue_provi
 import crmTimelineProjection from "../../../migrations/0074_crm_timeline_projection.sql";
 import analyticsPlatform from "../../../migrations/0075_analytics_platform.sql";
 import aiMemory from "../../../migrations/0076_ai_memory.sql";
+import seoGeoEngine from "../../../migrations/0077_seo_geo_engine.sql";
 import type { MigrationSource } from "@qooqnos/database";
 
 export const migrationSources: readonly MigrationSource[] = [
@@ -147,4 +148,5 @@ export const migrationSources: readonly MigrationSource[] = [
   { path: "migrations/0074_crm_timeline_projection.sql", sql: crmTimelineProjection },
   { path: "migrations/0075_analytics_platform.sql", sql: analyticsPlatform },
   { path: "migrations/0076_ai_memory.sql", sql: aiMemory },
+  { path: "migrations/0077_seo_geo_engine.sql", sql: seoGeoEngine },
 ];
