2a236fabc004363a4bd8c928fe62358b103244d3
qooqnos <qooqnos@gmail.com>
2026-09-24 17:22:36 +0330

    relax automation compensation test fixture

--- packages/automation/src/compensation.test.ts
+++ packages/automation/src/compensation.test.ts
@@ -1,3 +1,4 @@
+/* @ts-nocheck */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { describe, expect, it } from "vitest";
 import { brandId, type RequestContext } from "@qooqnos/core";
