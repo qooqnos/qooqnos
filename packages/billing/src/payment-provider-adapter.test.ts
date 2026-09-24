0abd3172e8ad4196c5821e07f786541e823b3b7d
qooqnos <qooqnos@gmail.com>
2026-09-24 17:29:42 +0330

    allow intentional test fixture type suppression

--- packages/billing/src/payment-provider-adapter.test.ts
+++ packages/billing/src/payment-provider-adapter.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 import { describe, expect, it } from "vitest";
 import { createHttpPaymentProviderAdapter, createPaymentProviderRegistry } from "./payment-provider-adapter";
