7406b33a9d4d49ecb2b9fab7eade18007d0d85d4
qooqnos <qooqnos@gmail.com>
2026-09-25 21:33:06 +0330

    feat(seo): export search intelligence control plane

--- packages/seo/src/index.ts
+++ packages/seo/src/index.ts
@@ -35,6 +35,7 @@ export * from "./competitive-repository";
 export * from "./indexnow";
 export * from "./search-engine-actions";
 export * from "./search-engine-gateway";
+export * from "./search-intelligence";
 
 export * from "./merchant-feed";
 export * from "./external-visibility-import";
